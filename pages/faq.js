23773387b3dc9eda358220cdf8e148eaa6763278
lucas-serra <lucasdserra1234@gmail.com>
2023-06-25 16:11:24 -0300

    Criando páginas e componentes.

---
+++ pages/faq.js
@@ -0,0 +1,10 @@
+import Link from "../src/components/Link";
+
+export default function FaqPage() {
+    return (
+        <div>
+            <h1>Alura Cases - Pagina de perguntas</h1>
+            <Link href="/">Ir para home</Link>
+        </div>
+    )
+}
